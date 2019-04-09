046c62d4a865f82a47bbe092e7502bce98408357
petrakovaru <petrakovaru@gmail.com>
2019-04-09 12:31:34 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function activatePlacesSearch(){
+    var input = document.getElementById('search_term');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+// search button function
+function search()
+{
+    var search_activate = document.getElementById('search_term').placeholder;
+
+}
